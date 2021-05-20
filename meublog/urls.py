b4fbebc30fd15919a14ea8adf93a6c2ab1ca5663
Carlos Henrique Alvarenga dos Santos <carlosho200@hotmail.com>
2021-05-19 21:10:10 -0400

    Criado forms e persitencia dos comentarios

--- meublog/urls.py
+++ meublog/urls.py
@@ -6,6 +6,8 @@ app_name = 'meublog'
 urlpatterns=[
     path('', views.ListarPostsView.as_view(), name='listar_posts'),
     path('<slug:slug>/<int:pk>', views.DetalharPostView.as_view(), name='detalhe'),
+    path('sharepost/<int:pk>/', views.FormContatoView.as_view(), name='share_post'),
+    path('comentarpost/<int:pk>/', views.FormComentarioView.as_view(), name='comentar_post'),
     #path('', views.listar_posts, name='listar_posts'),
     #path('<int:ano>/<int:mes>/<int:dia>/<slug:slug>/', views.detalhar_post, name='detalhe'),
 ]
